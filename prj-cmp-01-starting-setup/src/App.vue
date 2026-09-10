64a2e94ba431ffd0b2b81c1e655e4648fb12373f
Jahid Hasan <jahidmonpal911@gmail.com>
2026-09-10 07:57:49 +0600

    added first component and prop

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <ul></ul>
+  <ul>
+    <LearningResource
+      v-for="resource in storeResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></LearningResource>
+  </ul>
 </template>
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storeResources: [
         {
           id: 'offcicial-guide',
-          tite: 'Official Guide',
+          title: 'Official Guide',
           description:
             'The official Vue.js guide is a comprehensive resource for learning Vue.js, covering everything from the basics to advanced topics.',
           link: 'https://vuejs.org',
         },
         {
           id: 'google',
-          tite: 'Google',
+          title: 'Google',
           description:
             'Google is a search engine that provides a way to find information on the web.',
           link: 'https://google.com',
