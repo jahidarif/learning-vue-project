739acd5d562c3dd5d64440a2f298459d3b5da4d0
Jahid Hasan <jahidmonpal911@gmail.com>
2026-09-10 07:24:52 +0600

    Added dummy data for test

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul></ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'offcicial-guide',
+          tite: 'Official Guide',
+          description:
+            'The official Vue.js guide is a comprehensive resource for learning Vue.js, covering everything from the basics to advanced topics.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tite: 'Google',
+          description:
+            'Google is a search engine that provides a way to find information on the web.',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
